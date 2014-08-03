aadf280f7aa8f920820ec388ea4ffd846f738ac7
Pubudu Dissanayake <pubudu08@gmail.com>
2014-08-03 20:44:40 +0530

    Uploading document through http request

--- src/main/java/org/wso2/sample/doc/UploadDocument.java
+++ src/main/java/org/wso2/sample/doc/UploadDocument.java
@@ -1,13 +1,19 @@
 package org.wso2.sample.doc;
 
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.ContentBody;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.wso2.sample.doc.authentication.AuthenticationAdminServiceClient;
 import org.wso2.sample.doc.constants.APIConstantValues;
 import org.wso2.sample.doc.singleton.HttpClientSingleton;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -15,41 +21,54 @@ import java.io.IOException;
  */
 public class UploadDocument {
 
-	public static String ADD_DOC_API_CALL =
-			"https://localhost:9453/publisher/site/blocks/documentation/ajax/docs.jag?action=addDocumentation&mode='-'&provider=subscriber1&apiName=TestAPI&version=1.0.0&docName=testDoc&docType=how+to&sourceType=inline&docUrl='-'&summary=testing";
-	public static String LOGIN_API_CALL =
-			"https://localhost:9453/publisher/site/blocks/user/login/ajax/login.jag?action=login&username=admin&password=admin";
+	public static final String ADD_DOC_API_CALL =
+			"https://" + APIConstantValues.HOST_NAME + ":" + APIConstantValues.HTTPS_PORT +
+			"/publisher/site/blocks/documentation/ajax/docs.jag?action=addDocumentation&mode='-'&" +
+			"provider=subscriber1&apiName=TestDocAPI&version=1.0.0&docName=attachment&" +
+			"docType=how+to&sourceType=file&docUrl='-'&summary=testing";
+	public static final String LOGIN_API_CALL =
+			"https://" + APIConstantValues.HOST_NAME + ":" + APIConstantValues.HTTPS_PORT +
+			"/publisher/site/blocks/user/login/ajax/login.jag?action=login&username=" +
+			APIConstantValues.USER_NAME + "&password=" + APIConstantValues.PASSWORD;
+	public static final String FILE_PATH = "/home/pubudu/Desktop/t";
 
 	public static void main(String[] args) {
-		//String cookie = AccessToServer.signIn();
+
 		AuthenticationAdminServiceClient
 				.setSystemProperties(APIConstantValues.CLIENT_TRUST_STORE_PATH,
 				                     APIConstantValues.KEY_STORE_TYPE,
 				                     APIConstantValues.KEY_STORE_PASSWORD);
 		try {
-
 			HttpClient httpClient = HttpClientSingleton.getHttpClientInstance();
-			//HttpGet request = new HttpGet(API_CALL);
 			HttpPost request = new HttpPost(LOGIN_API_CALL);
 			HttpPost docRequest = new HttpPost(ADD_DOC_API_CALL);
-			request.addHeader("Content-Type", "application/x-www-form-urlencoded");
-			request.addHeader("Connection", "keep-alive");
-			// login to Publisher
 			HttpResponse response = httpClient.execute(request);
 			System.out.println("Http Status Code := " + response.getStatusLine());
 			for (Header header : response.getAllHeaders()) {
 				if (header.getName().equals("Set-Cookie")) {
 					docRequest.setHeader("Cookie", header.getValue().split(";", 2)[0]);
-					//System.out.println(header.getValue().split(";",2)[0]);
+					System.out.println(header.getValue().split(";", 2)[0]);
 				}
 				System.out.println(header);
 			}
 			System.out.println();
-			HttpResponse docResponse = httpClient.execute(docRequest);
-			System.out.println("Http Status Code := " + docResponse.getStatusLine());
-			for (Header header : docResponse.getAllHeaders()) {
+
+			File document = new File(FILE_PATH);
+			MultipartEntity multipartEntity = new MultipartEntity();
+			System.out.println(document.getAbsolutePath());
+			System.out.println();
+			multipartEntity.addPart("docLocation", new StringBody(document.getAbsolutePath()));
+			ContentBody fileBody = new FileBody(document, "text/plain");
+			multipartEntity.addPart("attachment", fileBody);
+			docRequest.setEntity(multipartEntity);
+
+			for (Header header : docRequest.getAllHeaders()) {
 				System.out.println(header);
 			}
+			System.out.println();
+			System.out.println("executing request " + docRequest.getRequestLine());
+			HttpResponse docResponse = httpClient.execute(docRequest);
+			System.out.println("Http Status Code := " + docResponse.getStatusLine());
 			System.out.println();
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/main/java/org/wso2/sample/doc/constants/APIConstantValues.java
+++ src/main/java/org/wso2/sample/doc/constants/APIConstantValues.java
@@ -7,17 +7,10 @@ public class APIConstantValues {
 
 	public static final String CLIENT_TRUST_STORE_PATH =
 			"/home/pubudu/Development/Workspace/Support/BANKOFAMERICADEV-21/Sample/DocumentAPI/src/main/resources/security/client-truststore.jks";
-
-
-
-
-	public static final String HOST_NAME = "10.134.65.143";
+	public static final String HOST_NAME = "localhost";
 	public static final String HTTPS_PORT = "9453";
-	public static final String SERVICE_URL =
-			"https://" + HOST_NAME + ":" + HTTPS_PORT + "/services/";
 	public static final String USER_NAME = "admin";
 	public static final String PASSWORD = "admin";
 	public static final String KEY_STORE_PASSWORD = "wso2carbon";
 	public static final String KEY_STORE_TYPE = "jks";
-	public static final String PUBLISHER_HOST = "10.134.65.143";
 }
